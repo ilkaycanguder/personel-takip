29606ee4bacd7f9395b1b1c88bf7499e68cffae2
ilkaycanguder <ilkaycanguder@gmail.com>
2025-02-13 17:24:51 +0300

    Enabled Swagger in production

--- backend-employee-management/Program.cs
+++ backend-employee-management/Program.cs
@@ -62,11 +62,11 @@ namespace backend_employee_management
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
